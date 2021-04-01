976a760b31fd7059d0c050bed394b7c41510a5b3
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-01 17:26:46 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="justify-center flex-1">
+    <div class="container pt-8 mx-auto">
+      <HelloWorld msg="Hello World" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  components: { HelloWorld },
+});
+</script>
