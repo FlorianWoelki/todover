b2e7483d8d6845d0e6ef5a86f94aa5c627d45a23
FlorianWoelki <FlorianWoelki@gmx.de>
2021-04-02 20:25:44 +0200

    ref(comp): change TodoItem to renderless component

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,11 +1,16 @@
-<template>
-  <input
-    class="w-full px-2 py-1 transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 focus:bg-gray-100"
-  />
-</template>
-
 <script lang="ts">
-import { defineComponent } from '@vue/runtime-core';
+import { defineComponent, h } from '@vue/runtime-core';
 
-export default defineComponent({});
+export default defineComponent({
+  render() {
+    return h(
+      'input',
+      {
+        class:
+          'w-full px-2 py-1 transition duration-150 ease-in-out focus:outline-none hover:bg-gray-100 focus:bg-gray-100',
+      },
+      { props: this.$attrs }
+    );
+  },
+});
 </script>
