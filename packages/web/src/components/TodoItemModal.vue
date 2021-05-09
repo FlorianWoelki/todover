d27c5704eacc96d23c62fa6b0181833606f60ea3
FlorianWoelki <FlorianWoelki@gmx.de>
2021-05-09 11:11:02 +0200

    chore(web): change padding and add xicon for todo item modal

--- packages/web/src/components/TodoItemModal.vue
+++ packages/web/src/components/TodoItemModal.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="absolute inset-0 z-50 h-screen" @click="hideButton">
     <div class="flex items-center justify-center h-full">
-      <div class="w-full max-w-xl p-4 bg-white rounded" @click.stop>
-        <h1 class="text-xl">{{ todoItem.name }}</h1>
+      <div class="w-full max-w-xl px-6 py-4 bg-white rounded" @click.stop>
+        <div class="flex items-center justify-between">
+          <h1 class="text-xl">{{ todoItem.name }}</h1>
+          <button class="text-gray-500 hover:text-gray-600 focus:outline-none" @click="hideButton">
+            <x-icon class="w-4 h-4"></x-icon>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -11,9 +16,13 @@
 <script lang="ts">
 import { defineComponent, PropType } from '@vue/runtime-core';
 import { Todo } from '../store/state';
+import XIcon from '@/assets/icons/x.svg';
 
 export default defineComponent({
   emits: ['hideButton'],
+  components: {
+    XIcon,
+  },
   props: {
     todoItem: {
       type: Object as PropType<Todo>,
