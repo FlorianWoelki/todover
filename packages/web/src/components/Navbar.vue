f84902d80c651c25966e9fdbb99cc649a3283b9a
FlorianWoelki <FlorianWoelki@gmx.de>
2021-05-17 08:48:05 +0200

    chore(web): change button and navbar appearance

--- packages/web/src/components/Navbar.vue
+++ packages/web/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-between px-4 py-2 text-white bg-gray-700 mb-14">
-    <h1 class="text-xl font-bold">todover</h1>
     <div class="flex items-center space-x-4">
+      <h1 class="text-xl font-bold">todover</h1>
       <div class="relative mx-auto text-gray-600">
         <input
           type="text"
@@ -29,7 +29,14 @@
           </svg>
         </button>
       </div>
-      <button type="button">Sign in</button>
+    </div>
+    <div class="flex items-center space-x-4">
+      <button type="button" class="rounded px-4 py-2 bg-gray-500 hover:bg-gray-600">
+        Register
+      </button>
+      <button type="button" class="rounded px-4 py-2 bg-transparent hover:bg-gray-800">
+        Sign in
+      </button>
     </div>
   </div>
 </template>
