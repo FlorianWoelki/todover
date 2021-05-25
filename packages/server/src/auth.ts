f688fb909e09b792b0d2c55e05c7c5e57ec15bf7
FlorianWoelki <FlorianWoelki@gmx.de>
2021-05-25 10:56:52 +0200

    fix: User type issues

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -1,5 +1,5 @@
+import { User } from '.prisma/client';
 import { sign } from 'jsonwebtoken';
-import { User } from './entities/User';
 
 export const createAccessToken = (user: User) => {
   return sign({ userId: user.id }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '15m' });
--- packages/server/src/resolvers/UserResolver.ts
+++ packages/server/src/resolvers/UserResolver.ts
@@ -9,7 +9,7 @@ import { sendRefreshToken } from '../sendRefreshToken';
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
-  users(@Ctx() { prisma }: MyContext): Promise<User[]> {
+  users(@Ctx() { prisma }: MyContext) {
     return prisma.user.findMany();
   }
 
@@ -35,7 +35,7 @@ export class UserResolver {
     @Ctx() { prisma }: MyContext,
     @Arg('email') email: string,
     @Arg('password') password: string
-  ): Promise<User> {
+  ) {
     const hashedPassword = await hash(password, 12);
 
     const user = prisma.user.create({ data: { email, password: hashedPassword } });
