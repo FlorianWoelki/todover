ebdaaae9c411998948e3fb4d616a4d03b73bd650
FlorianWoelki <FlorianWoelki@gmx.de>
2021-05-27 18:08:39 +0200

    test: add register mutation test

--- packages/server/__tests__/UserResolver.test.ts
+++ packages/server/__tests__/UserResolver.test.ts
@@ -1,15 +1,15 @@
 import 'reflect-metadata';
-import { PrismaClient } from '.prisma/client';
-import { User } from '.prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { ApolloServer, gql } from 'apollo-server-express';
 import { createTestClient } from 'apollo-server-testing';
 import { Request } from 'express';
-import { MockContext, MyContext, createMockContext } from '../src/MyContext';
+// import { MockContext, MyContext, createMockContext } from '../src/MyContext';
+import { cleanupUser } from './util/testUser';
 import { createSchema } from '../src/utils/createSchema';
 
 const prisma = new PrismaClient();
-let mockCtx: MockContext;
-let ctx: MyContext;
+// let mockCtx: MockContext;
+// let ctx: MyContext;
 // let request: Partial<Request>;
 
 // TODO: refactor to jest setup file
@@ -31,25 +31,29 @@ const REGISTER = gql`
   }
 `;
 
-const createUser = async (user: User, ctx: MyContext) => {
-  return await ctx.prisma.user.create({ data: user });
-};
-
 beforeEach(() => {
+  // mockCtx = createMockContext();
+  // ctx = (mockCtx as unknown) as MyContext;
+});
+
+let server: ApolloServer;
+
+beforeAll(async () => {
   // TODO: refactor to jest setup file
   process.env.DATABASE_URL =
     'postgresql://postgres:postgres@localhost:5432/clean-todo-app?schema=public';
   process.env.ACCESS_TOKEN_SECRET = 'accesstokensecret';
   process.env.REFRESH_TOKEN_SECRET = 'refreshtokensecret';
-
-  mockCtx = createMockContext();
-  ctx = (mockCtx as unknown) as MyContext;
+  server = (await constructTestServer()).server;
 });
 
 describe('Mutations', () => {
-  it('should create new user', async () => {
-    const { server } = await constructTestServer();
-    const { mutate } = createTestClient(server);
+  it('should register a new user', async () => {
+    // clean up user for new registering
+    await cleanupUser(prisma);
+
+    const mutate = createTestClient(server).mutate;
+
     const res = await mutate({
       mutation: REGISTER,
       variables: {
@@ -57,22 +61,8 @@ describe('Mutations', () => {
         password: '123',
       },
     });
-    console.log(res);
 
-    const user: User = {
-      id: 1,
-      email: 'test@test.de',
-      password: '123',
-      tokenVersion: null,
-    };
-
-    mockCtx.prisma.user.create.mockResolvedValue(user);
-
-    await expect(createUser(user, ctx)).resolves.toEqual({
-      id: 1,
-      email: 'test@test.de',
-      password: '123',
-      tokenVersion: null,
-    });
+    expect(res.errors).toBeUndefined();
+    expect(res.data?.register.email).toBe('test@test.de');
   });
 });
