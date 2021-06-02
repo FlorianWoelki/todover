5e9066c6388f61fd436b11f041da2d6d20c63d4d
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-02 17:10:30 +0200

    test: add change description test case

--- packages/server/__tests__/TodoResolver.test.ts
+++ packages/server/__tests__/TodoResolver.test.ts
@@ -218,6 +218,32 @@ describe('Mutations', () => {
     expect(res.data?.updateTodo.description).toBe(null);
     todo.repetition = 'weekly';
   });
+
+  it('updateTodo - change description', async () => {
+    server = (await constructTestServer(prisma, request)).server;
+    const mutate = createTestClient(server).mutate;
+    const description = 'Hello World Description';
+
+    const res = await mutate({
+      mutation: UPDATE_TODO,
+      variables: {
+        id: todo.id,
+        data: {
+          description,
+        },
+      },
+    });
+
+    expect(res.errors).toBeUndefined();
+    expect(res.data?.updateTodo.id).toBe(todo.id);
+    expect(res.data?.updateTodo.name).toBe(todo.name);
+    expect(res.data?.updateTodo.done).toBe(todo.done);
+    expect(res.data?.updateTodo.date).toBe(todo.date?.toISOString());
+    expect(res.data?.updateTodo.repetition).toBe(todo.repetition);
+    expect(res.data?.updateTodo.listId).toBe(null);
+    expect(res.data?.updateTodo.description).toBe(description);
+    todo.description = description;
+  });
 });
 
 describe('Queries', () => {
@@ -237,6 +263,6 @@ describe('Queries', () => {
     expect(res.data?.todos[0].date).toBe(todo.date?.toISOString());
     expect(res.data?.todos[0].repetition).toBe(todo.repetition);
     expect(res.data?.todos[0].listId).toBe(null);
-    expect(res.data?.todos[0].description).toBe(null);
+    expect(res.data?.todos[0].description).toBe(todo.description);
   });
 });
