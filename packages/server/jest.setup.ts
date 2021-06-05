2bcb91c377b7ccea73406439d16685e15db8dc77
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-05 17:13:22 +0200

    fix: database_url set in jest setup

--- packages/server/jest.setup.ts
+++ packages/server/jest.setup.ts
@@ -1,7 +1,5 @@
 import 'reflect-metadata';
 
 // set env variables that we need to connect o postgres db with prisma
-process.env.DATABASE_URL =
-  'postgresql://postgres:postgres@localhost:5432/clean-todo-app?schema=public';
 process.env.ACCESS_TOKEN_SECRET = 'accesstokensecret';
 process.env.REFRESH_TOKEN_SECRET = 'refreshtokensecret';
