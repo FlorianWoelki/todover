951bb0b44197b37e515213aeaff6ed7ac805556f
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-06 11:21:17 +0200

    test: fix local env variable for tests

--- packages/server/jest.setup.ts
+++ packages/server/jest.setup.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
 
 // set env variables that we need to connect o postgres db with prisma
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL =
+    'postgresql://postgres:postgres@localhost:5432/clean-todo-app?schema=public';
+}
 process.env.ACCESS_TOKEN_SECRET = 'accesstokensecret';
 process.env.REFRESH_TOKEN_SECRET = 'refreshtokensecret';
