468bc8d6b792a78560a3db7d57c7d1e14d660a79
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-08 18:55:19 +0200

    feat: add v-model compatibility for InputField

--- packages/ui/src/components/InputField/InputField.vue
+++ packages/ui/src/components/InputField/InputField.vue
@@ -4,6 +4,7 @@
     <input
       v-bind="$attrs"
       class="py-2 px-3 bg-white border border-gray-300 shadow-sm rounded-lg transition duration-100 ease-in-out focus:border-red-400 focus:ring-2 focus:ring-red-400 focus:outline-none focus:ring-opacity-50 focus:ring-offset-0"
+      @input="emitModelValue"
     />
   </div>
 </template>
@@ -13,11 +14,24 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   inheritAttrs: false,
+  emits: ['update:modelValue'],
   name: 'InputField',
   props: {
     label: {
       type: String,
     },
+    modelValue: {
+      type: String,
+    },
+  },
+  setup(_, { emit }) {
+    const emitModelValue = (event: InputEvent): void => {
+      emit('update:modelValue', (event.target as HTMLInputElement).value);
+    };
+
+    return {
+      emitModelValue,
+    };
   },
 });
 </script>
--- packages/web/src/views/Login.vue
+++ packages/web/src/views/Login.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="h-screen w-full flex items-center justify-center bg-gray-50">
-    <div class="bg-white shadow rounded-lg px-12 py-10 w-full max-w-lg space-y-6">
-      <input-field label="Email address" type="email" />
-      <input-field label="Password" type="password" />
+  <div class="h-screen w-full flex flex-col items-center justify-center bg-gray-50 -mt-12">
+    <img src="@/assets/logo.png" alt="logo" class="w-24 h-24 mb-4" />
+    <h1 class="text-3xl font-bold tracking-wide mb-12 text-gray-700">Sign in to your account</h1>
 
-      <td-button>Register</td-button>
+    <div class="bg-white shadow rounded-lg px-10 py-8 w-full max-w-lg space-y-6">
+      <input-field v-model="email" label="Email address" type="email" />
+      <input-field v-model="password" label="Password" type="password" />
+
+      <td-button @click="handleSignIn">Sign in</td-button>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  setup() {
+    const email = ref<string>('');
+    const password = ref<string>('');
+
+    const handleSignIn = (): void => {
+      console.log(email.value);
+      console.log(password.value);
+    };
+
+    return {
+      email,
+      password,
+      handleSignIn,
+    };
+  },
+});
+</script>
