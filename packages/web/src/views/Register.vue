2b6341345814dd123c6eba7c1c82a9bb8d9eff4b
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-11 15:26:28 +0200

    feat(view): add Register form

--- packages/web/src/views/Register.vue
+++ packages/web/src/views/Register.vue
@@ -1,7 +1,72 @@
-<template></template>
+<template>
+  <div class="h-screen w-full flex flex-col items-center justify-center bg-gray-50 -mt-12">
+    <img src="@/assets/logo.png" alt="logo" class="h-16 mb-2" />
+    <h1 class="text-3xl font-bold tracking-wide text-gray-700 mb-8">Create a new account</h1>
+    <router-link
+      to="/login"
+      class="mb-4 text-sm text-right text-gray-400 underline transition duration-100 ease-in-out hover:text-gray-500"
+    >
+      Have an account? No problem!
+    </router-link>
+
+    <div class="bg-white shadow rounded-lg px-10 py-8 w-full max-w-lg space-y-6">
+      <input-field
+        v-model="email"
+        label="Email address"
+        type="email"
+        @keydown.enter="handleSignUp"
+      />
+      <input-field
+        v-model="password"
+        label="Password"
+        type="password"
+        @keydown.enter="handleSignUp"
+      />
+      <input-field
+        v-model="repeatPassword"
+        label="Repeat password"
+        type="password"
+        @keydown.enter="handleSignUp"
+      />
+
+      <td-button :disabled="signupButtonDisabled" @click="handleSignUp">Create account</td-button>
+    </div>
+    <p v-if="showError" class="text-red-500 mt-4">Email address is already taken.</p>
+  </div>
+</template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent, ref } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const email = ref<string>('');
+    const password = ref<string>('');
+    const repeatPassword = ref<string>('');
+
+    const isEmailValid = (): boolean => {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email.value.toLowerCase());
+    };
+
+    const signupButtonDisabled = computed((): boolean => {
+      return (
+        email.value.length === 0 ||
+        password.value.length === 0 ||
+        password.value !== repeatPassword.value ||
+        !isEmailValid()
+      );
+    });
+
+    const handleSignUp = (): void => {};
+
+    return {
+      email,
+      password,
+      repeatPassword,
+      signupButtonDisabled,
+      handleSignUp,
+    };
+  },
+});
 </script>
