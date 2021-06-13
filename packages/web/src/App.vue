10272c4cbbf3cc6051d5fbf2bbc6ae228a97cbb8
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-13 16:48:30 +0200

    feat: add handling for expiring access token

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -9,8 +9,9 @@
 <script lang="ts">
 import { defineComponent, onMounted, provide, ref } from '@vue/runtime-core';
 import { DefaultApolloClient } from '@vue/apollo-composable';
-import { apolloClient } from './apollo-client';
 import { useStore } from 'vuex';
+import jwtDecode, { JwtPayload } from 'jwt-decode';
+import { apolloClient } from './apollo-client';
 import { Mutation } from './store';
 
 export default defineComponent({
@@ -20,14 +21,34 @@ export default defineComponent({
     provide(DefaultApolloClient, apolloClient(store));
 
     onMounted(async () => {
-      // TODO: change to env variable
-      const { accessToken } = await fetch('http://localhost:4000/refresh_token', {
-        method: 'POST',
-        credentials: 'include',
-      }).then((res) => res.json());
+      const token = store.state.user.accessToken;
 
-      store.commit(Mutation.SET_ACCESS_TOKEN, accessToken);
-      loading.value = false;
+      if (!token) {
+        // TODO: change to env variable
+        const { accessToken } = await fetch('http://localhost:4000/refresh_token', {
+          method: 'POST',
+          credentials: 'include',
+        }).then((res) => res.json());
+
+        store.commit(Mutation.SET_ACCESS_TOKEN, accessToken);
+        loading.value = false;
+      } else {
+        try {
+          const { exp } = jwtDecode<JwtPayload>(token);
+          if (exp && Date.now() >= exp * 1000) {
+            // TODO: change to env variable
+            const { accessToken } = await fetch('http://localhost:4000/refresh_token', {
+              method: 'POST',
+              credentials: 'include',
+            }).then((res) => res.json());
+
+            store.commit(Mutation.SET_ACCESS_TOKEN, accessToken);
+            loading.value = false;
+          }
+        } catch (error) {
+          console.log('refresh token expired', error);
+        }
+      }
     });
 
     return {
