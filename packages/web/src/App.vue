05cf06bc6c00d4fed7c424f738ece80a00c8f70c
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-13 16:18:26 +0200

    feat: refreshing accessToken and loading screen

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <navbar v-if="$route.path === '/'" class="font-sans antialiased" />
-  <router-view />
+  <div v-if="loading">loading...</div>
+  <template v-else>
+    <navbar v-if="$route.path === '/'" class="font-sans antialiased" />
+    <router-view />
+  </template>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, provide } from '@vue/runtime-core';
+import { defineComponent, onMounted, provide, ref } from '@vue/runtime-core';
 import { DefaultApolloClient } from '@vue/apollo-composable';
 import { apolloClient } from './apollo-client';
 import { useStore } from 'vuex';
+import { Mutation } from './store';
 
 export default defineComponent({
   setup() {
     const store = useStore();
+    const loading = ref<boolean>(true);
     provide(DefaultApolloClient, apolloClient(store));
 
     onMounted(async () => {
-      const response = await fetch('http://localhost:4000/refresh_token', {
+      // TODO: change to env variable
+      const { accessToken } = await fetch('http://localhost:4000/refresh_token', {
         method: 'POST',
         credentials: 'include',
       }).then((res) => res.json());
-      console.log(response);
+
+      store.commit(Mutation.SET_ACCESS_TOKEN, accessToken);
+      loading.value = false;
     });
+
+    return {
+      loading,
+    };
   },
 });
 </script>
