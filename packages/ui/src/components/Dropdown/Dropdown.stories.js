ff5f1c54498b3876e2661047c8d3b99430386226
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-17 16:25:34 +0200

    docs(ui): add first dropdown story

---
+++ packages/ui/src/components/Dropdown/Dropdown.stories.js
@@ -0,0 +1,13 @@
+import MyDropdown from './Dropdown.vue';
+
+export default {
+  title: 'UI/Dropdown',
+  component: MyDropdown,
+};
+
+const Template = (args) => ({
+  components: { MyDropdown },
+  template: '<my-dropdown><p>Test Item 1</p><p>Test Item 2</p></my-dropdown>',
+});
+
+export const Default = Template.bind({});
