ca0e6d083efffa7bf08a54aa5d7f0bf9fd9f3bed
FlorianWoelki <FlorianWoelki@gmx.de>
2021-06-24 19:26:55 +0200

    feat: add i18n package (#7)

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
+import messages from '@intlify/vite-plugin-vue-i18n/messages';
 import UI from '@todover/ui/src/index';
 import { store } from './store';
 import router from './router';
 import App from './App.vue';
 import './index.css';
 
-createApp(App).use(UI).use(router).use(store).mount('#app');
+const i18n = createI18n({
+  legacy: false,
+  locale: 'en',
+  globalInjection: true,
+  messages,
+});
+
+createApp(App).use(i18n).use(UI).use(router).use(store).mount('#app');
--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,12 +1,18 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
-import Vue from '@vitejs/plugin-vue';
-import ViteComponents from 'vite-plugin-components';
+import vue from '@vitejs/plugin-vue';
+import viteComponents from 'vite-plugin-components';
 import svgLoader from 'vite-svg-loader';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 import pkg from './package.json';
 
 export default defineConfig({
-  plugins: [Vue(), ViteComponents(), svgLoader()],
+  plugins: [
+    vue(),
+    viteComponents(),
+    svgLoader(),
+    vueI18n({ include: path.resolve(__dirname, '../i18n/locales/**') }),
+  ],
   optimizeDeps: {
     include: ['@apollo/client/core', '@apollo/client/link/context'],
     exclude: ['@apollo/client'],
