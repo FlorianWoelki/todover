109b366235d46378e2117322a49feddeda2a00cb
FlorianWoelki <FlorianWoelki@gmx.de>
2021-07-03 10:55:19 +0200

    feat(comp): add language options for settings modal (#17)

--- packages/web/src/components/modals/SettingsModal.vue
+++ packages/web/src/components/modals/SettingsModal.vue
@@ -4,8 +4,63 @@
       <h2 class="text-xl">{{ $t('settings.title') }}</h2>
     </template>
 
-    <div class="flex items-center space-x-4">
-      <p>Language</p>
+    <div class="flex items-center space-x-12">
+      <p class="text-gray-500">Language</p>
+      <div class="relative">
+        <div
+          class="px-4 py-2 text-gray-600 transition duration-100 ease-in-out bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200"
+          @click="dropdownHidden = false"
+        >
+          English
+        </div>
+
+        <dropdown
+          #="{ itemClasses }"
+          class="right-0 bg-white"
+          :dropdownHidden="dropdownHidden"
+          @close.stop="dropdownHidden = true"
+        >
+          <div
+            v-for="(language, index) in validLanguages"
+            :key="index"
+            class="text-gray-800 hover:bg-gray-100"
+            :class="itemClasses"
+          >
+            {{ language.name }}
+            <span class="text-gray-500">({{ language.slug }})</span>
+          </div>
+        </dropdown>
+      </div>
     </div>
   </modal>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+interface Language {
+  slug: string;
+  name: string;
+}
+
+export default defineComponent({
+  setup() {
+    const validLanguages: Language[] = [
+      {
+        slug: 'en',
+        name: 'English',
+      },
+      {
+        slug: 'de',
+        name: 'German',
+      },
+    ];
+    const dropdownHidden = ref<boolean>(true);
+
+    return {
+      dropdownHidden,
+      validLanguages,
+    };
+  },
+});
+</script>
