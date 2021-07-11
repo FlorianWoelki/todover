ce3ef02633ef8055c426643db5e2140349549db6
FlorianWoelki <FlorianWoelki@gmx.de>
2021-07-11 18:05:15 +0200

    fix(ui): storybook not opening caused by i18n

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,4 +1,8 @@
+import { app } from '@storybook/vue3';
+import { i18n } from '@todover/i18n/src/i18n';
 import '../tailwind.css';
+
+app.use(i18n);
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
