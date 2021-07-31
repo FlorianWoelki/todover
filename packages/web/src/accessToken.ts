26a0aa1a871c3f5984a2f192ad0bc2cbdf9c8e4f
FlorianWoelki <FlorianWoelki@gmx.de>
2021-07-31 18:31:14 +0200

    docs(web): add jsdoc to  file

--- packages/web/src/accessToken.ts
+++ packages/web/src/accessToken.ts
@@ -4,14 +4,33 @@ import { Mutation, State } from './store';
 
 let accessToken = '';
 
+/**
+ * Set the access token to the new specified parameter.
+ *
+ * @param {string} accessToken - The new access token.
+ * @returns {void}
+ */
 export const setAccessToken = (token: string): void => {
   accessToken = token;
 };
 
+/**
+ * Get the access token.
+ *
+ * @returns {string} - The access token.
+ */
 export const getAccessToken = (): string => {
   return accessToken;
 };
 
+/**
+ * Try to refresh the access token based on the refresh token in the cookies.
+ * This function will update the token with @see {@link setAccessToken} and executes
+ * @see {@link Mutation.SET_LOADING} to disable the loading status.
+ *
+ * @param {Store} store - The Vuex store.
+ * @returns {void}
+ */
 export const tryToRefreshAccessToken = async (store: Store<State>): Promise<void> => {
   const token = getAccessToken();
 
