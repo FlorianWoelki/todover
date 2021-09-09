828df77dd9e38d8d3a1e2a1d343a646851048f70
FlorianWoelki <FlorianWoelki@gmx.de>
2021-09-09 18:55:58 +0200

    fix(server): production deployment allows subdomains

--- packages/server/src/sendRefreshToken.ts
+++ packages/server/src/sendRefreshToken.ts
@@ -1,5 +1,9 @@
 import { Response } from 'express';
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.cookie('jid', token, { httpOnly: true, secure: true });
+  if (process.env.NODE_ENV === 'production') {
+    res.cookie('jid', token, { httpOnly: true, secure: true, domain: '.todover.com' });
+  } else {
+    res.cookie('jid', token, { httpOnly: true, secure: true });
+  }
 };
