eef70a233fa76e66e3bba54fd5befbe1b2accd73
FlorianWoelki <FlorianWoelki@gmx.de>
2021-09-09 16:18:25 +0200

    build(server): add deploy-server script

---
+++ packages/server/deploy-server.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+docker build -t florianwoelki/todover-server:1 .
+docker push florianwoelki/todover-server:1
+ssh root@68.183.71.58 "docker pull florianwoelki/todover-server:1 && docker tag florianwoelki/todover-server:1 dokku/backend:latest && dokku deploy backend latest"
