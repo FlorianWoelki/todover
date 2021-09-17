cff6942feac90a15ae3b313032c32699fae6b036
FlorianWoelki <FlorianWoelki@gmx.de>
2021-09-17 13:39:09 +0200

    refactor(server): change lifetime of refreshToken to 30 days

--- packages/server/src/auth.ts
+++ packages/server/src/auth.ts
@@ -11,6 +11,6 @@ export const createRefreshToken = (user: User) => {
   return sign(
     { userId: user.id, tokenVersion: user.tokenVersion },
     process.env.REFRESH_TOKEN_SECRET!,
-    { expiresIn: '7d' }
+    { expiresIn: '30d' }
   );
 };
