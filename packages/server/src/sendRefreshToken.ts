baca04bddbd0ce151844eb0e023756609408d0da
FlorianWoelki <FlorianWoelki@gmx.de>
2021-10-16 12:17:36 +0200

    fix(server): expiration date for refreshToken

--- packages/server/src/sendRefreshToken.ts
+++ packages/server/src/sendRefreshToken.ts
@@ -3,10 +3,16 @@ import { Response } from 'express';
 const cookieIdentifier = 'jid';
 
 export const sendRefreshToken = (res: Response, token: string) => {
+  const expirationDate = new Date(Date.now() + 30 * 24 * 3600 * 1000);
   if (process.env.NODE_ENV === 'production') {
-    res.cookie(cookieIdentifier, token, { httpOnly: true, secure: true, domain: '.todover.com' });
+    res.cookie(cookieIdentifier, token, {
+      httpOnly: true,
+      secure: true,
+      domain: '.todover.com',
+      expires: expirationDate,
+    });
   } else {
-    res.cookie(cookieIdentifier, token, { httpOnly: true, secure: true });
+    res.cookie(cookieIdentifier, token, { expires: expirationDate });
   }
 };
 
